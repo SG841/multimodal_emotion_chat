6de189faabdb7d874c152b06322e18c5f11c4e5d
lizeming <19917621825@163.com>
2026-05-21 12:58:14 +0800

    Remove deleted fusion module import

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,8 +4,7 @@
 
 from . import vision
 from . import audio
-from . import fusion
 from . import llm
 from . import tts
 
-__all__ = ['vision', 'audio', 'fusion', 'llm', 'tts']
+__all__ = ['vision', 'audio', 'llm', 'tts']
